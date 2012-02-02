574605b0f99d9ec1352c880866c360d65713d2c2
Alexey Illarionov <littlesavage@rambler.ru>
2012-02-03 02:44:06 +0400

    Dump0

--- output_rinex_nav.c
+++ output_rinex_nav.c
@@ -1,9 +1,10 @@
 
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include <math.h>
 
 #include "gpsd/gps.h"
 #include "sirfdump.h"
@@ -12,13 +13,21 @@
 
 extern const char const *MonthName[];
 
-#define L1_CARRIER_FREQ 1575420000.0
-#define SPEED_OF_LIGHT 299792458.0
-#define GPS_EPOCH  315964800 /*  GPS epoch in Unix time */
+#define MAX_GPS_PRN  32
 
 struct tm0 {
    unsigned year, month, day, hour, min;
    float sec;
+};
+
+struct nav_data_t {
+   unsigned is_printed;
+   unsigned is_sub1_active;
+   unsigned is_sub2_active;
+   unsigned is_sub3_active;
+   struct subframe_t sub1;
+   struct subframe_t sub2;
+   struct subframe_t sub3;
 };
 
 struct rinex_nav_ctx_t {
@@ -31,10 +40,14 @@ struct rinex_nav_ctx_t {
 
    int header_printed;
 
+   struct nav_data_t data[MAX_GPS_PRN];
 };
 
-static int print_obs_header(FILE *out_f, struct rinex_nav_ctx_t *ctx);
 int gpstime2tm0(unsigned gps_week, double gps_tow, struct tm0 *res);
+
+
+static int print_nav_header(FILE *out_f, const struct rinex_nav_ctx_t *ctx);
+static int print_nav_data(FILE *out_f, const struct nav_data_t *nav_data);
 static int handle_mid8_msg(struct rinex_nav_ctx_t *ctx,
       tSIRF_MSG_SSB_50BPS_DATA *msg,
       FILE *out_f);
@@ -43,6 +56,7 @@ void *new_rinex_nav_ctx(int argc, char **argv)
 {
    struct rinex_nav_ctx_t *ctx;
    struct tm *tm;
+   unsigned i;
    time_t clock;
 
    if (argc || argv) {};
@@ -61,6 +75,13 @@ void *new_rinex_nav_ctx(int argc, char **argv)
 	 tm->tm_hour, tm->tm_min);
 
    ctx->header_printed = 0;
+
+   for(i=0; i < MAX_GPS_PRN; i++) {
+      ctx->data[i].is_sub1_active =
+	 ctx->data[i].is_sub2_active =
+	 ctx->data[i].is_sub3_active =
+	 ctx->data[i].is_printed = 0;
+   }
 
    return ctx;
 }
@@ -115,6 +136,8 @@ static int handle_mid8_msg(struct rinex_nav_ctx_t *ctx,
 {
    unsigned i;
    struct subframe_t subp;
+   struct subframe_t *dst_subp;
+   struct nav_data_t *dst;
    uint32_t words[10];
 
    assert(ctx);
@@ -124,33 +147,91 @@ static int handle_mid8_msg(struct rinex_nav_ctx_t *ctx,
    for(i=0; i<10; i++)
       words[i]=msg->word[i];
 
-   /*
-   fprintf(stderr, "msg 8 (50 BPS data). sv_id: %2d data: "
-	 "%08lx %08lx %08lx %08lx %08lx %08lx %08lx %08lx %08lx %08lx\n"
-	 ,
-	 (unsigned)msg->svid,
-	 msg->word[0], msg->word[1], msg->word[2], msg->word[3],
-	 msg->word[4], msg->word[5], msg->word[6], msg->word[7],
-	 msg->word[8], msg->word[9]
-	 );
-	 */
    if (gpsd_interpret_subframe_raw(&subp, msg->svid, words) <= 0)
       return -1;
 
-   if (!ctx->header_printed)
-      ctx->header_printed = print_obs_header(out_f, ctx);
+   assert(subp.tSVID <= MAX_GPS_PRN);
+
+   if (subp.subframe_num > 3)
+      return 0;
+
+   dst = &ctx->data[subp.tSVID-1];
+
+   switch (subp.subframe_num) {
+      case 1:
+	 if (dst->is_sub1_active
+	       && (dst->sub1.sub1.IODC == dst->sub1.sub1.IODC))
+	    return 0;
+
+	 memcpy(&dst->sub1, &subp, sizeof(subp));
+	 dst->is_sub1_active = 1;
+	 if (dst->is_sub2_active
+	       && ((dst->sub1.sub1.IODC & 0xff) != dst->sub2.sub2.IODE))
+	    dst->is_printed = dst->is_sub2_active = 0;
+	 if (dst->is_sub3_active
+	       && ((dst->sub1.sub1.IODC & 0xff) != dst->sub3.sub3.IODE))
+	    dst->is_printed = dst->is_sub3_active = 0;
+	 break;
+      case 2:
+	 if (dst->is_sub2_active
+	       && (dst->sub2.sub2.IODE == subp.sub2.IODE))
+	    /*  skip subframe */
+	       break;
+
+	 memcpy(&dst->sub2, &subp, sizeof(subp));
+	 dst->is_sub2_active = 1;
+	 if (dst->is_sub1_active
+	       && ((dst->sub1.sub1.IODC & 0xff) != dst->sub2.sub2.IODE))
+	    dst->is_printed = dst->is_sub1_active = 0;
+	 if (dst->is_sub3_active
+	       && (dst->sub3.sub3.IODE != dst->sub2.sub2.IODE))
+	       dst->is_printed = dst->is_sub3_active = 0;
+	 break;
+      case 3:
+	 if (dst->is_sub3_active
+	       && (dst->sub3.sub3.IODE == subp.sub3.IODE))
+	    /*  skip subframe */
+	       break;
+
+	 memcpy(&dst->sub3, &subp, sizeof(subp));
+	 dst->is_sub3_active = 1;
+	 if (dst->is_sub1_active
+	       && ((dst->sub1.sub1.IODC & 0xff) != dst->sub3.sub3.IODE))
+	    dst->is_printed = dst->is_sub1_active = 0;
+	 if (dst->is_sub2_active
+	       && (dst->sub2.sub2.IODE != dst->sub3.sub3.IODE))
+	       dst->is_printed = dst->is_sub2_active = 0;
+	 break;
+      default:
+	 return 0;
+	 break;
+   }
+
+   if (!dst->is_printed
+	 && dst->is_sub1_active
+	 && dst->is_sub2_active
+	 && dst->is_sub3_active
+	 && ((dst->sub1.sub1.IODC & 0xff) == dst->sub2.sub2.IODE)
+	 && ((dst->sub1.sub1.IODC & 0xff) == dst->sub3.sub3.IODE)
+	 ) {
+
+      if (!ctx->header_printed)
+	 ctx->header_printed = print_nav_header(out_f, ctx);
+
+      dst->is_printed =  print_nav_data(out_f, dst);
+   }
 
    return 0;
 }
 
-static int print_obs_header(FILE *out_f, struct rinex_nav_ctx_t *ctx)
+static int print_nav_header(FILE *out_f, const struct rinex_nav_ctx_t *ctx)
 {
 
    assert(ctx);
    assert(out_f);
 
    fprintf(out_f, "%9.2f%-11s%-20s%-20s%-20s\r\n",
-	 2.11,
+	 2.10,
 	 "",
 	 "NAVIGATION DATA",
 	 "",
@@ -165,6 +246,77 @@ static int print_obs_header(FILE *out_f, struct rinex_nav_ctx_t *ctx)
 }
 
 
+static int print_nav_data(FILE *out_f, const struct nav_data_t *nav_data)
+{
+   unsigned wn;
+   struct tm0 epoch;
+
+   assert(nav_data);
+
+   /* XXX */
+   wn = nav_data->sub1.sub1.WN+1024;
+   gpstime2tm0(wn, nav_data->sub1.sub1.l_toc, &epoch);
+
+   fprintf(out_f,
+	 "%2u%3u%3u%3u%3u%3u%5.1f%19.12lf%19.12lf%19.12lf\r\n",
+	 nav_data->sub1.tSVID,
+	 epoch.year % 100,
+	 epoch.month,
+	 epoch.day,
+	 epoch.hour,
+	 epoch.min,
+	 epoch.sec,
+	 /*  XXX */
+	 nav_data->sub1.sub1.d_af0,
+	 nav_data->sub1.sub1.d_af1,
+	 nav_data->sub1.sub1.d_af2);
+
+   fprintf(out_f,
+	 "   %19.12lE%19.12lE%19.12lE%19.12lE\r\n"
+	 "   %19.12lE%19.12lE%19.12lE%19.12lE\r\n"
+	 "   %19.12lE%19.12lE%19.12lE%19.12lE\r\n"
+	 "   %19.12lE%19.12lE%19.12lE%19.12lE\r\n"
+	 "   %19.12lE%19.12lE%19.12lE%19.12lE\r\n"
+	 "   %19.12lE%19.12lE%19.12lE%19.12lE\r\n"
+	 "   %19.12lE%19.12lE\r\n",
+	 (double)nav_data->sub2.sub2.IODE,
+	 nav_data->sub2.sub2.d_Crs,
+	 nav_data->sub2.sub2.d_deltan * (double)GPS_PI,
+	 /* XXX  */
+	 nav_data->sub2.sub2.d_M0,
+
+	 nav_data->sub2.sub2.d_Cuc,
+	 nav_data->sub2.sub2.d_eccentricity,
+	 nav_data->sub2.sub2.d_Cus,
+	 nav_data->sub2.sub2.d_sqrtA,
+
+	 (double)nav_data->sub2.sub2.l_toe,
+	 nav_data->sub3.sub3.d_Cic,
+	 nav_data->sub3.sub3.d_Omega0 * (double)GPS_PI,
+	 nav_data->sub3.sub3.d_Cis,
+
+	 nav_data->sub3.sub3.d_i0 * (double)GPS_PI,
+	 nav_data->sub3.sub3.d_Crc,
+	 nav_data->sub3.sub3.d_omega * (double)GPS_PI,
+	 nav_data->sub3.sub3.d_Omegad * (double)GPS_PI,
+
+	 nav_data->sub3.sub3.d_IDOT * (double)GPS_PI,
+	 (double)nav_data->sub1.sub1.l2,
+	 (double)wn,
+	 (double)nav_data->sub1.sub1.l2p,
+
+	 (double)nav_data->sub1.sub1.ura,
+	 (double)(nav_data->sub1.sub1.hlth & 0x40),
+	 nav_data->sub1.sub1.d_Tgd,
+	 (double)nav_data->sub1.sub1.IODC,
+
+	 /* XXX  */
+	 (double)nav_data->sub1.sub1.l_toc,
+	 0.0
+	 );
+
+   return 1;
+}
 
 
 
